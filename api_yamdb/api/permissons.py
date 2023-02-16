70419699c1bf824a57bb80b2628745957be2129a
Даниил Алексеенко <115031171+cra1ger51@users.noreply.github.com>
2023-02-16 21:00:54 +0300

    Update permissons.py

--- api_yamdb/api/permissons.py
+++ api_yamdb/api/permissons.py
@@ -1,9 +1,20 @@
-from rest_framework import permissions
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
-class IsAdminOrSuperuserPermission(permissions.BasePermission):
+class IsAdminOrSuperuserPermission(BasePermission):
 
     def has_permission(self, request, view):
         if request.user.role == 'admin' or request.user.is_superuser:
             return True
         return False
+
+
+class CustomPermission(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method not in SAFE_METHODS:
+            if request.method == 'POST' and request.user.is_authenticated == True:
+                return True
+            elif obj.author == request.user or request.user.role in ('moderator','admin'):
+                return True
+        else:
+            return True

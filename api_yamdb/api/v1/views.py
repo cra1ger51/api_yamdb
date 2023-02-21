573f5796f94d6c4085b072b57556f4d157169999
Даниил Алексеенко <cra1ger@bk.ru>
2023-02-21 22:59:42 +0300

    PEP8 Iteration #2

--- api_yamdb/api/v1/views.py
+++ api_yamdb/api/v1/views.py
@@ -73,11 +73,9 @@ class CommentViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticatedOrReadOnly, CustomPermission)
 
     def get_queryset(self):
-        review = get_object_or_404(
-                                   Review,
+        review = get_object_or_404(Review,
                                    pk=self.kwargs.get('review_id'),
-                                   title=self.kwargs.get('title_id')
-                                  )
+                                   title=self.kwargs.get('title_id'))
         return review.comments.all()
 
     def perform_create(self, serializer):
